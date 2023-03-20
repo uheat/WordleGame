41f1e6d7eaa036208744b077749f696717c50749
uheat <uheat@kakao.com>
2023-03-20 23:37:54 +0900

    중간저장

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
-(function(){
+// (function(){
+// })();
     
-})();
+let inputNum = 1;
+
+window.onload = function(){
+    moveFocus(inputNum);
+}
+
+function moveFocus(curNum){
+    const guessFirst = document.querySelector(`input[data-index="${curNum}"]`);
+    guessFirst.addEventListener("keyup",function(event){
+        if(curNum == inputNum){
+            inputNum++;
+            document.querySelector(`input[data-index="${inputNum}"]`).focus();
+        }
+    })
+}
