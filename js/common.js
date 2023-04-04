3c7272033d6a7b26a19b1b072ef7ff4d443329c0
uheat <uheat@kakao.com>
2023-04-04 23:45:59 +0900

    정답체크 로직 추가함

--- js/common.js
+++ js/common.js
@@ -2,17 +2,54 @@
 // })();
     
 let inputNum = 1;
+let spellCount = 5;
+let questName = 'guess-first'
+const answer = 'truck';
 
-window.onload = function(){
-    moveFocus(inputNum);
-}
+const guessBtn = document.querySelector('#guess-btn');
+const historybox = document.querySelector('#history');
+const guessInputs = document.querySelectorAll(`#${questName} input`);
 
-function moveFocus(curNum){
-    const guessFirst = document.querySelector(`input[data-index="${curNum}"]`);
+guessBtn.addEventListener('click',function(){
+
+    checkAnswer();
+})
+
+
+function moveFocus(currentNum){
+    const guessFirst = document.querySelector(`input[data-index="${currentNum}"]`);
     guessFirst.addEventListener("keyup",function(event){
-        if(curNum == inputNum){
-            inputNum++;
-            document.querySelector(`input[data-index="${inputNum}"]`).focus();
+        if(currentNum < spellCount){
+            document.querySelector(`input[data-index="${currentNum*1+1}"]`).focus();
         }
     })
+}
+
+function checkAnswer(){
+    let inputAnswer = getInputAnswer();
+    appendAnswer(inputAnswer);
+
+    if(inputAnswer == answer){
+        alert('정답')
+    }else{
+        alert('오답')
+    }
+}
+
+function getInputAnswer(){
+    
+    let guessAnswer = '';
+
+    guessInputs.forEach(function(element) {
+        guessAnswer += element.value
+    });
+
+    return guessAnswer;
+}
+
+
+function appendAnswer(inputAnswer){
+    let li = document.createElement('li');
+    li.append(inputAnswer);
+    historybox.append(li);
 }
